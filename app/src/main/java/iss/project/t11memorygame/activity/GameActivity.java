603d85e2accd2e89adadbafc483663a66d85e938
HarrisonTheG <harrison28590@gmail.com>
2021-07-09 22:12:27 +0800

    Added Android App architecture with namings and simple layout for Home Activity

---
+++ app/src/main/java/iss/project/t11memorygame/activity/GameActivity.java
@@ -0,0 +1,16 @@
+package iss.project.t11memorygame.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import iss.project.t11memorygame.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
