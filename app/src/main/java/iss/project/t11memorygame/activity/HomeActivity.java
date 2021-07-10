5f2aa5e85db0b45575f779a6c594c9623665544f
HarrisonTheG <harrison28590@gmail.com>
2021-07-10 13:18:03 +0800

    Add home screen layout and design, link play button to next screen, add flip dependency

--- app/src/main/java/iss/project/t11memorygame/activity/HomeActivity.java
+++ app/src/main/java/iss/project/t11memorygame/activity/HomeActivity.java
@@ -2,15 +2,32 @@ package iss.project.t11memorygame.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import iss.project.t11memorygame.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //register play button
+        Button playButton = (Button) findViewById(R.id.play);
+        playButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.play){
+            Intent intent = new Intent(this, SearchImageActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
